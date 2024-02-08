293752f3bc2214b69cbf1978cc90a683e50fe326
ahamedcad01 <ahamedcad01@gmail.com>
2024-02-08 16:17:13 +0530

    files updated

---
+++ js/myscript.js
@@ -0,0 +1,54 @@
+<<<<<<< HEAD
+function  addition()
+{
+    var a=8
+   var b=20
+var c=10
+ console.log(a);
+ console.log(a+b);
+ console.log(a+b+c);
+console.log("added values of : "+ (a+b+c));
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+var a =15
+if (a<10)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+
+}
+}
+ addition()
+=======
+function  addition()
+{
+    var a=8
+   var b=20
+var c=10
+ console.log(a);
+ console.log(a+b);
+ console.log(a+b+c);
+console.log("added values of : "+ (a+b+c));
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+var a =15
+if (a<10)
+{
+    console.log("true");
+}
+else
+{
+    console.log("false");
+
+}
+}
+ addition()
+>>>>>>> de3abdb236053023a32c5b6342edbd7ecdef555b
+ 
